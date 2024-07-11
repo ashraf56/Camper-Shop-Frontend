619d4f6ebb01d56b494daf20302f24f6a2aa7a5d
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-11 16:06:21 +0600

    navbar code formated

--- src/page/Navbar.tsx
+++ src/page/Navbar.tsx
@@ -11,8 +11,8 @@ import {
     SheetDescription,
     SheetHeader,
     SheetTrigger,
-  } from "@/components/ui/sheet"
-  const Navbar=() => {
+} from "@/components/ui/sheet"
+const Navbar = () => {
     return (
         <header className="flex h-14 w-full shrink-0 items-center bg-black bg-opacity-5 px-4 md:px-6 absolute z-20">
             <Link to={'/'} className="mr-6 flex">
@@ -75,71 +75,71 @@ import {
 
 
 
- {/* this code   for mobile and tablet device */}
+            {/* this code   for mobile and tablet device */}
             <Sheet>
-      <SheetTrigger asChild >
-        <Button variant="outline" className="block lg:hidden ml-auto">
-        <img src={menu} width={20} height={20} />
-        </Button>
-      </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetDescription>
-          <nav className="ml-auto flex flex-col gap-6">
-                <Button variant={'outline'} size={"default"} >
-                    <Link to={'/'}
-                    >
-                        Home
-                    </Link>
-                </Button>
-                <Button variant={'outline'} size={"default"} >
-                    <Link to={'/about'}
-                    >
-                        About us
-                    </Link>
-                </Button>
-                <Button variant={'outline'} size={"default"} >
-                    <Link to={'/products'}
-                    >
-                        ALL Products
-                    </Link>
-                </Button>
-                <Button variant={'outline'} size={"default"} >
-                    <Link to={'/products'}
-                    >
-                        Products Management
-                    </Link>
-                </Button>
+                <SheetTrigger asChild >
+                    <Button variant="outline" className="block lg:hidden ml-auto">
+                        <img src={menu} width={20} height={20} />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetDescription>
+                            <nav className="ml-auto flex flex-col gap-6">
+                                <Button variant={'outline'} size={"default"} >
+                                    <Link to={'/'}
+                                    >
+                                        Home
+                                    </Link>
+                                </Button>
+                                <Button variant={'outline'} size={"default"} >
+                                    <Link to={'/about'}
+                                    >
+                                        About us
+                                    </Link>
+                                </Button>
+                                <Button variant={'outline'} size={"default"} >
+                                    <Link to={'/products'}
+                                    >
+                                        ALL Products
+                                    </Link>
+                                </Button>
+                                <Button variant={'outline'} size={"default"} >
+                                    <Link to={'/products'}
+                                    >
+                                        Products Management
+                                    </Link>
+                                </Button>
 
 
 
-            </nav>
-            <nav className="ml-auto flex flex-col gap-3">
-                <Button variant={'outline'} size={"default"} >
-                    <Link to={'/products'}
-                    >
-                        <div className="flex gap-2">
-                            <img src={order} width={20} height={20} />
-                            <p>12</p>
-                        </div>
-                    </Link>
-                </Button>
-                <Button variant={'outline'} size={"default"} >
-                    <Link to={'/products'}
-                    >
-                        <div className="flex gap-2">
-                            <img src={cart} width={20} height={20} />
-                            <p>12</p>
-                        </div>
-                    </Link>
-                </Button>
+                            </nav>
+                            <nav className="ml-auto flex flex-col gap-3">
+                                <Button variant={'outline'} size={"default"} >
+                                    <Link to={'/products'}
+                                    >
+                                        <div className="flex gap-2">
+                                            <img src={order} width={20} height={20} />
+                                            <p>12</p>
+                                        </div>
+                                    </Link>
+                                </Button>
+                                <Button variant={'outline'} size={"default"} >
+                                    <Link to={'/products'}
+                                    >
+                                        <div className="flex gap-2">
+                                            <img src={cart} width={20} height={20} />
+                                            <p>12</p>
+                                        </div>
+                                    </Link>
+                                </Button>
 
-            </nav>
-          </SheetDescription>
-        </SheetHeader>
-        
-      </SheetContent>
-    </Sheet>
+                            </nav>
+                        </SheetDescription>
+                    </SheetHeader>
+
+                </SheetContent>
+            </Sheet>
 
 
         </header>
