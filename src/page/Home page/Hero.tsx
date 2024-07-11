f912e4c2d380502aa233ea19a02610b105b1d8a6
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-11 13:02:54 +0600

    navbar customized

--- src/page/Home page/Hero.tsx
+++ src/page/Home page/Hero.tsx
@@ -3,17 +3,17 @@ import Herocard from './Herocard';
 
 const Hero = () => {
     return (
-        <div className='w-full h-[90vh] flex flex-col relative items-center justify-center  ' style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)),url(${imgs})`, backgroundSize:'cover', backgroundPosition:'bottom' }}>
-         <div className='mb-9'>
-            <p className='text-center text-lg font-light font-CustomFont text-white '> Explore </p>
-           <h1 className='text-white font-bold font-CustomFont uppercase text-7xl text-center '>Camp gear</h1>
-         </div>
-           
-           
-<div className='absolute bottom-20'>
-<Herocard></Herocard>
+        <div className='w-full h-[90vh] flex flex-col relative items-center justify-center ' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)),url(${imgs})`, backgroundSize: 'cover', backgroundPosition: 'bottom' , objectFit:'contain'}}>
+            <div className='mb-32'>
+                <p className='text-center text-lg font-light font-CustomFont tracking-[0.2em]'> Explore </p>
+                <h1 className='font-bold font-CustomFont uppercase text-7xl text-center tracking-[0.2em]'>Campgear</h1>
+            </div>
 
-</div>
+
+            <div className='absolute bottom-20'>
+                <Herocard></Herocard>
+
+            </div>
 
 
         </div>
--- src/page/Navbar.tsx
+++ src/page/Navbar.tsx
@@ -1,29 +1,44 @@
 
  import { Button } from "@/components/ui/button"
 import { Link } from "react-router-dom"
+import logo from "@/assets/mountain.png"
 
 export default function Component() {
   return (
     <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 absolute z-20">
             <Link to={'/'} className="mr-6 hidden lg:flex">
-            
-       <span className="">Acme Inc</span>
+      <div className="flex items-center justify-center gap-2">
+      <div><img src={logo} width={40} height={40}/></div>
+      <p className="text-black font-CustomFont font-extrabold uppercase text-2xl tracking-widest">CampGear</p>
+      </div>
          
       </Link>
-      <nav className="ml-auto hidden lg:flex gap-6">
-        <Button size={"default"} className="bg-transparent">
+      <nav className="m-auto hidden lg:flex gap-6">
+        <Button variant={'outline'} size={"default"} color="black" className="bg-transparent">
         <Link to={'/'}
         >
           Home
         </Link>
         </Button>
-        <Button size={"default"} className="bg-transparent">
+        <Button variant={'outline'} size={"default"} className="bg-transparent">
         <Link to={'/about'}
         >
           about us
         </Link>
         </Button>
-        <Button size={"default"} className="bg-transparent">
+        <Button variant={'outline'} size={"default"} className="bg-transparent">
+        <Link to={'/products'}
+        >
+          products
+        </Link>
+        </Button>
+        <Button  variant={'outline'} size={"default"} className="bg-transparent">
+        <Link to={'/products'}
+        >
+          products
+        </Link>
+        </Button>
+        <Button variant={'outline'} size={"default"} className="bg-transparent">
         <Link to={'/products'}
         >
           products
@@ -32,6 +47,17 @@ export default function Component() {
     
        
       </nav>
+      <nav className="ml-auto hidden lg:flex gap-6">
+        <Button size={"default"} color="black" className="bg-transparent">
+        <Link to={'/'}
+        >
+          Home
+        </Link>
+        </Button>
+       
+       
+      </nav>
+
     </header>
   )
 }
