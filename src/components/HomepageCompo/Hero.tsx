94b34aeb98cc487b89e27b12332ded7cdd96bbd8
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-12 12:04:23 +0600

    home page component organized

--- src/components/HomepageCompo/Hero.tsx
+++ src/components/HomepageCompo/Hero.tsx
@@ -3,7 +3,7 @@ import Herocard from './Herocard';
 
 const Hero = () => {
     return (
-        <div className='w-full h-[90vh] flex flex-col relative items-center justify-center ' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.1)),url(${imgs})`, backgroundSize: 'cover', backgroundPosition: 'bottom' , objectFit:'contain'}}>
+        <div className='w-full h-[90vh] flex flex-col relative items-center justify-center ' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.1)),url(${imgs})`, backgroundSize: 'cover', backgroundPosition: 'bottom', objectFit: 'contain' }}>
             <div className='mb-5 lg:mb-28 text-white'>
                 {/* <p className='text-center text-lg font-light font-CustomFont tracking-[0.2em]'> Explore </p> */}
                 <p className='  text-center text-lg font-light font-CustomFont uppercase tracking-[0.2em]'> Your One-Stop Camping Shop </p>
--- src/page/Home page/Home.tsx
+++ src/page/Home page/Home.tsx
@@ -1,7 +1,7 @@
-import Categories from "./Categories";
-import { Faq } from "./Faq";
-import FeatureProducta from "./FeatureProducta";
-import Hero from "./Hero";
+import Categories from "../../components/HomepageCompo/Categories";
+import { Faq } from "../../components/HomepageCompo/Faq";
+import FeatureProducta from "../../components/HomepageCompo/FeatureProducta";
+import Hero from "../../components/HomepageCompo/Hero";
 import RecommendedProducts from "./RecommandedProducts";
 import UniqueSection from "./UniqueSection";
 
