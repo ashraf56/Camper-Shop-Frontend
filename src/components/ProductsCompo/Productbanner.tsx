60a203053b3681de83dc92b097ef1d2675c80845
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-13 11:49:58 +0600

    about and product page baner updated

--- src/components/ProductsCompo/Productbanner.tsx
+++ src/components/ProductsCompo/Productbanner.tsx
@@ -1,9 +1,9 @@
-import imgs from '@/assets/abouthero.jpg'
+import imgs from '@/assets/randombanner2.jpg'
 
 const Productbanner = () => {
     return (
         <div>
-            <div className='w-full h-[50vh] flex flex-col relative items-center justify-center ' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.1)),url(${imgs})`, backgroundSize: 'cover', backgroundPosition: 'top', objectFit: 'contain' }}>
+            <div className='w-full h-[50vh] flex flex-col relative items-center justify-center ' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)),url(${imgs})`, backgroundSize: 'cover', backgroundPosition: 'bottom ', objectFit: 'contain' }}>
                 <div className=' text-white'>
 
                     <h1 className='font-bold font-CustomFont uppercase text-3xl md:text-7xl text-center tracking-[0.2em]'>Our Product</h1>
