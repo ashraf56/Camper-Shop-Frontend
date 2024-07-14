a9d8011c3b854b481c651ef177309ccada90dd2b
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-14 11:21:13 +0600

    redux added

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routers/router.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={store}>
 
-<RouterProvider router={router} />
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
